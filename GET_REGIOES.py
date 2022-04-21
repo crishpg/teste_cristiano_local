428979039fed24f4d6d018458c3be4109e081c98
Cristiano Schmitt <cristianoschmitt@MacBook-Pro-de-Cristiano.local>
2022-04-21 13:37:37 -0300

    primeiro commit

--- GET_REGIOES.py
+++ GET_REGIOES.py
@@ -3,6 +3,7 @@ import requests
 url = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes?orderBy=nome'
 headers = {'Accept': 'application/json'}
 #auth = ('username', 'userpass')
+print ('teste primeiro commit')
 response = requests.get(url, headers=headers)
 
 with open('regioes.json', 'wb') as outf:
